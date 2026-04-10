ce9a318ab79b765de6c11560fde1176c55cf6213
Prasanna <laxmiprasanna73374@gmail.com>
2026-04-10 23:17:33 +0530

    update

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { setupCounter } from './counter.ts'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div class="container">
     <h1>🚀 My First Website</h1>
-    <h2>By Prasanna</h2>
+    <h2>By Prasanna G</h2>
     <p>Successfully deployed on Vercel 🎉</p>
 
     <button id="counter" class="btn">Click Me</button>
